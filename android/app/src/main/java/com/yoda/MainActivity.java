ae0297f7279d507e6c0e6fe9a9438550d8f6137c
MechanicKim <mechanic0406@gmail.com>
2016-11-14 14:35:04 +0900

    Merged with master

--- android/app/src/main/java/com/yoda/MainActivity.java
+++ android/app/src/main/java/com/yoda/MainActivity.java
@@ -3,14 +3,9 @@ package com.yoda;
 import android.os.Bundle;
 import android.content.Intent;
 import com.facebook.react.ReactActivity;
-<<<<<<< HEAD
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-=======
->>>>>>> 8722772ee98f291ef3c8940ecf9f42af60d0639a
 
 public class MainActivity extends ReactActivity {
 
--- src/utils/UrlMeta.js
+++ src/utils/UrlMeta.js
@@ -2,7 +2,7 @@ class UrlMeta {
 
   //Host and API
   static host = 'http://ec2-52-78-121-221.ap-northeast-2.compute.amazonaws.com:8000/';
-  // host = 'http:/192.168.0.22:8000/';
+  //host = 'http://192.168.0.111:8000/';
 
   static API_LOGIN = 'users/signIn/';
   static API_MENTOR = 'users/mentorlist/';
@@ -14,7 +14,7 @@ class UrlMeta {
   static API_MENTOR_RESP = 'match/response/';
   static API_EDIT_GENERAL = 'users/editGeneral/';
   static API_LOCAL_SIGNUP = 'users/localSignUp/';
-  static API_LOCAL_SIGNIN = 'users/localSignIn/'
+  static API_LOCAL_SIGNIN = 'users/localSignIn/';
   static API_SECRET_CODE = 'users/secretCode/';
   static API_RESET_PASS = 'users/resetPassword/';
   static API_EDIT_PERSONALITY = 'users/editPersonality/';
