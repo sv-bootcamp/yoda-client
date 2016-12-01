a9b49ef37bf7f88c65e13b2e9ed5199d3a50eb89
Mingyu Kim <mechanic0406@gmail.com>
2016-12-01 15:53:32 +0900

    Fixed commented errors

--- ios/Yoda/SplashViewController.m
+++ ios/Yoda/SplashViewController.m
@@ -16,8 +16,7 @@
 @implementation SplashViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  [super viewDidLoad];
   
   NSURL *url = [[NSBundle mainBundle] URLForResource:@"splash_anim" withExtension:@"gif"];
   NSData *data = [NSData dataWithContentsOfURL:url];
@@ -29,18 +28,7 @@
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
