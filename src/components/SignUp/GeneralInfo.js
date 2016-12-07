c604ae9c78d9c7727b3502303d5746e816084ce2
Mingyu Kim <mechanic0406@gmail.com>
2016-12-07 18:07:51 +0900

    Fixed image upload problem

--- src/components/SignUp/GeneralInfo.js
+++ src/components/SignUp/GeneralInfo.js
@@ -282,13 +282,15 @@ class GeneralInfo extends Component {
       image,
     };
 
-    UserUtil.editGeneral(this.onUploadCallback.bind(this), fieldSet);
+    console.log(fieldSet);
+
+    //UserUtil.editGeneral(this.onUploadCallback.bind(this), fieldSet);
   }
 
   // Render progress bar, profile image and form.
   render() {
     const readyUploadImage = (imageResource) => {
-      this.state.imageResource = imageResource;
+      this.state.profile.imageResource = imageResource;
     };
 
     const Forms = this.getForms();
