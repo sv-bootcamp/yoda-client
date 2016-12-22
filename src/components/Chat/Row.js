cb99e59c1a29fe8473e041256077126b1e383002
Dabin Kim <heartjigi@gmail.com>
2016-12-22 11:57:13 +0900

    rebase chat dim

--- src/components/Chat/Row.js
+++ src/components/Chat/Row.js
@@ -8,7 +8,6 @@ import {
   View,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { dimensions } from '../Shared/Dimensions';
 import moment from 'moment/min/moment.min';
 import Text from '../Shared/UniText';
 
@@ -92,7 +91,7 @@ export default class Row extends Component {
       <TouchableHighlight underlayColor='lightgray' onPress={this.goToChat}>
         <View style={styles.row}>
           <Image style={styles.photo}
-            source={{ uri: this.state.opponent.profileUrl }}/>
+                 source={{ uri: this.state.opponent.profileUrl }}/>
           <View style={styles.userInformation}>
             <View style={styles.leftSection}>
               <Text style={styles.name}>
@@ -127,13 +126,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   photo: {
-    height: dimensions.fontWeight * 40,
-    width: dimensions.fontWeight * 40,
-    marginTop: dimensions.heightWeight * 15,
-    marginBottom: dimensions.heightWeight * 15,
-    marginLeft: dimensions.widthWeight * 15,
-    marginRight: dimensions.widthWeight * 15,
-    borderRadius: dimensions.fontWeight * 20,
+    height: 40,
+    width: 40,
+    margin: 15,
+    borderRadius: 20,
   },
   userInformation: {
     flex: 1,
@@ -148,52 +144,47 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
   },
   name: {
-    fontSize: dimensions.fontWeight * 14,
+    fontSize: 14,
     fontWeight: 'bold',
-    marginTop: dimensions.heightWeight * 18,
+    marginTop: 18,
     color: '#494b4c',
   },
   lastMessage: {
-    fontSize: dimensions.fontWeight * 12,
-    marginTop: dimensions.heightWeight * 8,
-    marginBottom: dimensions.heightWeight * 18,
+    fontSize: 12,
+    marginTop: 8,
+    marginBottom: 18,
     fontWeight: 'normal',
     color: '#a6aeae',
   },
   rightSection: {
-    width: dimensions.widthWeight * 90,
+    width: 90,
     alignItems: 'flex-end',
-    paddingRight: dimensions.widthWeight * 15,
+    paddingRight: 15,
   },
   rightTopSection: {
-    marginTop: dimensions.heightWeight * 10,
+    marginTop: 10,
     flexDirection: 'row',
-    alignItems: 'center',
   },
   lastTimestamp: {
-    fontSize: dimensions.fontWeight * 10,
+    fontSize: 10,
+    paddingRight: 10,
+    lineHeight: 10,
     fontWeight: 'normal',
     color: '#a6aeae',
   },
   unreadCountContainer: {
-    width: dimensions.widthWeight * 31,
-    height: dimensions.heightWeight * 20,
-    marginTop: dimensions.heightWeight * 17,
+    width: 31,
+    height: 20,
+    marginTop: 17,
     borderRadius: 40,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fd5b52',
   },
   unreadCountText: {
-    fontSize: dimensions.fontWeight * 12,
+    fontSize: 12,
     color: '#ffffff',
     letterSpacing: -0.3,
     backgroundColor: 'transparent',
-  },
-  onboardingImage: {
-    width: dimensions.widthWeight * 8,
-    height: dimensions.heightWeight * 13,
-    marginLeft: dimensions.widthWeight * 10,
-    resizeMode: 'contain',
   },
 });
